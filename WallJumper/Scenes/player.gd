94a0cd64c95476c994132804a64e7c2df0d33151
Karacraft <kcraftpk@gmail.com>
2025-03-10 22:48:19 +0500

    removed some stuff

--- WallJumper/Scenes/player.gd
+++ WallJumper/Scenes/player.gd
@@ -2,15 +2,18 @@ extends CharacterBody2D
 
 # States for State Machine
 enum States{ AIR = 1, FLOOR, WALL}
+enum Direction { RIGHT = 1, NONE = 0, LEFT = -1}
 # Player in space (check main its floating)
 var state = States.AIR
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@export_group("Game Internal Physics")
 @export var SPEED = 210
 @export var JUMP_FORCE = -480
 @export var WALL_JUMP_FORCE = 480
 @export var WALL_SLIDE_GRAVITY = 480
-var direction = 1 # 1 right, 0 none, -1 left
+var direction = Direction.RIGHT
 
 func _physics_process(delta):
 #	Set Direciton
@@ -40,7 +43,7 @@ func _physics_process(delta):
 			else:
 				$AnimatedSprite2D.play("wall")
 			# Add Wall Jump
-			if Input.is_action_pressed("jump") and ((Input.is_action_pressed("left") and direction == 1 ) or ( Input.is_action_pressed("right") and direction == -1)):
+			if Input.is_action_pressed("jump") and ((Input.is_action_pressed("left") and direction == Direction.RIGHT ) or ( Input.is_action_pressed("right") and direction == -1)):
 				velocity.x = 200 * -direction
 				velocity.y = 480
 				state = States.AIR
@@ -64,10 +67,10 @@ func user_input():
 	direction = Input.get_axis("left","right")
 
 	velocity.x = SPEED * direction
-	if direction == -1:
+	if direction == Direction.LEFT:
 		$AnimatedSprite2D.play("walk")
 		$AnimatedSprite2D.flip_h = true
-	elif direction == 1:
+	elif direction == Direction.RIGHT:
 		$AnimatedSprite2D.play("walk")
 		$AnimatedSprite2D.flip_h = false
 	else:
@@ -78,6 +81,6 @@ func is_near_wall():
 	return $WallChecker.is_colliding()
 
 func set_direction():
-	direction = 1 if not $AnimatedSprite2D.flip_h else -1
+	direction = Direction.RIGHT if not $AnimatedSprite2D.flip_h else Direction.LEFT
 	$WallChecker.rotation_degrees = 90 * -direction
 	pass
