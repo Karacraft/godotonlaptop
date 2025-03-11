1ca1898a7f1dc0aa639abdd6b08a80150a6d073d
Karacraft <kcraftpk@gmail.com>
2025-03-11 19:57:19 +0500

    some code

--- isometric/scripts/character_body_2d.gd
+++ isometric/scripts/character_body_2d.gd
@@ -2,10 +2,17 @@ extends CharacterBody2D
 
 @export_group("Movement Variables")
 @export var color_aween : Color = "#fff030"
-@export var some_texture : Texture2D
 @export var speed: float = 200.0
 @export var run_multiplier = 1.5
+@export_subgroup("Movim","Images_")
+@export var left_image : Texture2D
+@export var right_image : Texture2D
+@export var up_image : Texture2D
+@export var down_image : Texture2D
 #var velocity = Vector2.ZERO
+
+func _init() -> void:
+	pass
 
 func _process(delta: float) -> void:
 	var input_vector = Vector2.ZERO
@@ -20,12 +27,16 @@ func _process(delta: float) -> void:
 
 	if Input.is_action_pressed("up"):
 		input_vector.y -= 1
+		$Sprite2D.texture = up_image
 	if Input.is_action_pressed("down"):
 		input_vector.y += 1
+		$Sprite2D.texture = down_image
 	if Input.is_action_pressed("left"):
 		input_vector.x -= 1
+		$Sprite2D.texture = left_image
 	if Input.is_action_pressed("right"):
 		input_vector.x += 1
+		$Sprite2D.texture = right_image
 
 	# Normalize input vector to prevent faster diagonal movement
 	input_vector = input_vector.normalized()
